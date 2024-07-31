cf7bb3775daa68969314e5ef0062ee76b9b77608
s-sweta <121677567+s-sweta@users.noreply.github.com>
2024-07-31 15:36:37 +0000

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { config } from 'dotenv';
 
 config();
@@ -7,8 +7,13 @@ config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-
-
-
+  build: {
+    outDir: 'dist', // Ensure this matches the deployment folder
+    rollupOptions: {
+      input: {
+        main: 'index.html' // Ensure this matches your entry point
+      }
+    }
+  },
+  base: '/' // Adjust this if deploying to a subpath
+});
